48a098d4eb1dceaf0676e29a99c61a8e78c4b22d
Haeeul <etoilune00@gmail.com>
2020-04-08 01:24:46 +0900

    A+B 5번 풀이

--- Algorithm_java/src/IO/A_plus_B.java
+++ Algorithm_java/src/IO/A_plus_B.java
@@ -5,16 +5,36 @@ public class A_plus_B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//A+B - 3
+//		Scanner scanner = new Scanner(System.in);
+//	      int x, a, b;
+//	      System.out.print("실행할 연산 횟수를 입력하세요 :");
+//	      x = scanner.nextInt();
+//	      for(int i=0; i<x; i++) {
+//	         //
+//	         System.out.print("더하기를 수행할 두 숫자를 입력하세요 :");
+//	         a = scanner.nextInt();
+//	         b = scanner.nextInt();
+//	         System.out.println(a + b);
+//	      }
+		
+		// A+B -5
 		Scanner scanner = new Scanner(System.in);
-	      int x, a, b;
-	      System.out.print("실행할 연산 횟수를 입력하세요 :");
-	      x = scanner.nextInt();
-	      for(int i=0; i<x; i++) {
-	         //
-	         System.out.print("더하기를 수행할 두 숫자를 입력하세요 :");
-	         a = scanner.nextInt();
-	         b = scanner.nextInt();
-	         System.out.println(a + b);
+	    int a, b;
+	    int i = 0;
+	    int x[] = new int[100];
+	    while(true){
+	    	System.out.print("더하기를 수행할 두 숫자를 입력하세요 :");
+		    a = scanner.nextInt();
+		    b = scanner.nextInt();
+		    if(a !=0 || b!=0) {
+		    	x[i] = a+b;
+		    	i++;
+		    }
+		    else break;
+	    }
+	    for(int j=0; j<=i; j++) {
+	       System.out.println(x[j]);
 	      }
 	}
 
