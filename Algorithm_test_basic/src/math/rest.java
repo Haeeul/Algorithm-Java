151b69b8dc193efb9ebacbd1f3ee2be55a0f996d
Haeeul <etoilune00@gmail.com>
2020-04-15 02:40:06 +0900

    #10430_수학_나머지_풀이

---
+++ Algorithm_test_basic/src/math/rest.java
@@ -0,0 +1,22 @@
+package math;
+
+import java.util.Scanner;
+
+public class rest {
+
+	public static void main(String[] args) {
+		//#10430_수학_나머지
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		sc.close();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+	}
+
+}
