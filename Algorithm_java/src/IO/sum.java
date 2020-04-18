bf20d494fa756cc218b53084c24425abfb61cd34
Haeeul <etoilune00@gmail.com>
2020-04-19 02:45:35 +0900

    #11720_숫자의 합_풀이 충돌 수정.

--- Algorithm_java/src/IO/sum.java
+++ Algorithm_java/src/IO/sum.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class sum {
 	public static void main(String[] args) {
-		//11720_숫자의 합
-		Scanner scanner = new Scanner(System.in);
+		//#11720_숫자의 합
 		int sum = 0;
+		
+		Scanner scanner = new Scanner(System.in);
 		int a = scanner.nextInt();
 		String st = scanner.next();
 		
 		for(int i = 0; i<a; i++) {
 			sum += Integer.parseInt(st.substring(i,i+1));
 		}
+		
 		scanner.close();
 		System.out.println(sum);
 	}
