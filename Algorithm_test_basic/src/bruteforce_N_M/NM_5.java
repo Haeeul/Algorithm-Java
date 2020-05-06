6417f1b46d803cd1e042d5ac4d3ed3895e8b138e
Haeeul <etoilune00@gmail.com>
2020-05-06 23:20:31 +0900

    #15654번_N과 M_5번_풀이

---
+++ Algorithm_test_basic/src/bruteforce_N_M/NM_5.java
@@ -0,0 +1,45 @@
+package bruteforce_N_M;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NM_5 {
+	static boolean[] c = new boolean[10];
+	static int [] a = new int[10];
+	static int [] num;
+	
+	public static void go(int index, int n, int m) {
+		if(index==m) {
+			for(int i =0; i<m; i++) {
+				System.out.print(a[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i =0; i<n; i++) {
+			if(c[i]) continue;
+			c[i] = true;
+			a[index] = num[i];
+			go(index+1, n, m);
+			c[i] = false;
+		}
+	}
+
+	public static void main(String[] args) {
+		//#15654¹ø_N°ú M_5¹ø
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		num =  new int[n];
+		
+		for(int i =0; i<n; i++) {
+			num[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		Arrays.sort(num);
+		go(0,n,m);
+	}
+}
