503e38ce500fe1e45c66d644eae2d829d79a3d3e
Haeeul <etoilune00@gmail.com>
2020-07-24 02:38:07 +0900

    #13398_출력값만 맞음_수정필요

--- Algorithm_test_basic/src/dynamic/Q13398.java
+++ Algorithm_test_basic/src/dynamic/Q13398.java
@@ -4,39 +4,37 @@ import java.util.Scanner;
 
 public class Q13398 {
 	public static void main(String[] args) {
-		//#13398 ¿¬¼ÓÇÕ2
+		//#13398 Â¿Â¬Â¼Ã“Ã‡Ã•2
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int[] num = new int[n+1];
-		
+
 		for(int i =1; i<=n; i++) {
 			num[i] = sc.nextInt();
 		}
-		
+
 		int[] left = new int[n+2];
 		int[] right = new int[n+2];
 		int ans = num[1];
-		
+
 		for(int i=1; i<=n; i++) {
-			left[i] = num[i];
-			left[i] = Math.max(left[i], left[i-1]+num[i]);
+			left[i] = Math.max(num[i], left[i-1]+num[i]);
 			if(left[i]>ans) {
 				ans = left[i];
 			}
 		}
-		
+
 		for(int i=n; i>=1; i--) {
-			right[i] = num[i];
-			right[i] = Math.max(right[i], right[i+1]+num[i]);
+			right[i] = Math.max(num[i], right[i+1]+num[i]);
 		}
-		
+
 		for(int i=1; i<=n; i++) {
 			if(left[i-1]+right[i+1]>ans) {
 				ans = left[i-1]+right[i+1];
 			}
 		}
-		
+
 		System.out.print(ans);
 	}
 }
